a27bf44bccd3ab634323b430c1105130239d0bbb
Kai-Yin Huang <h.kate97089@gmail.com>
2023-05-12 19:06:55 +0800

    update

---
+++ 3/hw3.py
@@ -0,0 +1,37 @@
+
+###############################################
+#                                             #
+#        Regular Expression Exercises         #
+#                                             #
+###############################################
+
+x = 10    # An integer
+
+y = 3.14  # A floating number
+
+print('\#') # 測試如何避免
+            # 誤刪非註解的
+            # 字串 '\#'
+
+z = """This is
+not a comment,
+it is a string!
+"""
+print("\nz =", z)
+
+'''''
+這是多行註解一
+
+'''
+"""
+這是多行註解二
+說明
+
+"""
+#
+#
+w = '''這不是註解，
+這是另一個字串！
+'''
+print("\nw = ", w)
+input("press enter key to exit...")
